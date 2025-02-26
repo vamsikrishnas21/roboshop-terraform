25a9aaeb3539ccd16270aaa4ca400045f4454460
Vamsi Krishna S <karthikvamsi21@gmail.com>
2025-02-26 10:14:04 +0530

    Roboshop code

--- frontend.tf
+++ frontend.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-049982f50bac9e830"]
 
   tags = {
     Name = "frontend"
